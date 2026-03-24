922bee4375972271f3a606c67ddaf6d8f9d8ebf3
Madhumitha212 <madhuravi324@gmail.com>
2026-03-24 15:38:35 +0530

    add: [Madhu]-added SQLAlchemy for user table

---
+++ src/models/user_model.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+ 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index = True)
+    name = Column(String(100))
+    email = Column(String(150), unique=True, nullable = False)
+    password = Column(String(200), nullable = False)
+    contact_no = Column(String(15), nullable=True)
+    is_active = Column(Boolean, default=False)
