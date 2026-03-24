1ceedf4c9a792fdb729d03b9ac3c57639f712a9e
Madhumitha212 <madhuravi324@gmail.com>
2026-03-24 19:27:28 +0530

    update: [Madhu]-updated main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from src.config.database_conn import engine
 from src.models.user_model import Base
-from src.router.user_route import router
+from src.router.user_route import router as user_router
 from src.config.logging_config import *
 
 logger = setup_logger()
@@ -9,4 +9,4 @@ app = FastAPI()
 
 Base.metadata.create_all(bind = engine)
 
-app.include_router(router)
+app.include_router(user_router)
